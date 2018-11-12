5d80bec01b159fa927b9c5fa31d64bbcf81363a8
aljarrell <alan.k.jarrell@gmail.com>
2018-11-12 16:55:37 -0500

    added get for lucky number two

--- app.rb
+++ app.rb
@@ -63,6 +63,14 @@ post '/lucky_number_one' do
   age = params[:age]
   last_name = params[:lname]
   first_name = params[:fname]
-  redirect '/lucky_number_two?fname=' + first_name + '&lname=' + last_name + '&age=' + age + '&fav_color=' + fav_color
+  redirect '/lucky_number_two?fname=' + first_name + '&lname=' + last_name + '&age=' + age + '&fav_color=' + fav_color + '&lucky_number_one=' + lucky_number_one
+end
 
+get '/lucky_number_two' do
+  lucky_number_one = params[:lucky_number_one]
+  fav_color = params[:fav_color]
+  age = params[:age]
+  last_name = params[:lname]
+  first_name = params[:fname]
+  erb :lucky_number_two, locals: {last_name: last_name, first_name: first_name, age: age, fav_color: fav_color, lucky_number_one: lucky_number_one}
 end
